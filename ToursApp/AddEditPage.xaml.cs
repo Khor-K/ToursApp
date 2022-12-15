702cacaeed4b91de22a1ee4727c9d7844a543d30
Khor-K <mr.chuuvak@gmail.com>
2022-12-15 12:00:10 +0300

    Добавьте файлы проекта.

---
+++ ToursApp/AddEditPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+
+        public AddEditPage()
+        {
+            InitializeComponent();
+            DataContext = _currentHotel;
+            ComboCountries.ItemsSource = ToursEntities.GetContext().Country.ToList();
+        }
+
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+            ComboCountries.ItemsSource = ToursEntities.GetContext().Country.ToList();
+            DataContext = _contentLoaded;
+            if (selectedHotel != null)
+            {
+                _currentHotel = selectedHotel;
+                NameT.Text = selectedHotel.name;
+                ComboCountries.SelectedValue = _currentHotel.Country;
+                StarsT.Text = selectedHotel.countOfStars.ToString();
+            }
+            
+        }
+
+        private void BtnSave_Click_1(object sender, RoutedEventArgs e)
+        {
+            _currentHotel.Country = (Country)ComboCountries.SelectedValue;
+
+            _currentHotel.countOfStars = int.Parse(StarsT.Text);
+            _currentHotel.name = NameT.Text;
+
+            StringBuilder errors = new StringBuilder();
+            
+            if (string.IsNullOrWhiteSpace(_currentHotel.name))
+                errors.AppendLine("Укажите название отеля ");
+            if (_currentHotel.countOfStars < 1 || _currentHotel.countOfStars > 5)
+                errors.AppendLine("Количество звёзд 1 - число от до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentHotel.id == null)
+            {
+                _currentHotel.id = ToursEntities.GetContext().Hotel.DefaultIfEmpty().Max(r => r == null ? 0 : r.id) + 1;
+                ToursEntities.GetContext().Hotel.Add(_currentHotel);
+            }
+                
+
+            try
+            {
+                ToursEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ ToursApp/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ImportTours();
+            MainFrame.Navigate(new HostelsPage());
+            //MainFrame.Navigate(new ToursPage());
+            Manager.MainFrame = MainFrame;
+        }
+
+        private void ImportTours()
+        {
+            var fileData = File.ReadAllLines(@"C:\Users\mrchu\Documents\Khoroshilov\Туры.txt");
+            var images = Directory.GetFiles(@"C:\Users\mrchu\Documents\Khoroshilov\Туры фото");
+
+            foreach (var line in fileData)
+            {
+                var data = line.Split('\t');
+                var tempTour = new Tour
+                {
+                    name = data[0].Replace("\"",""),
+                    countryId = data[1].Replace(" ", ""),
+                    ticketsCount = int.Parse(data[2]),
+                    price = (int)decimal.Parse(data[3]),
+                    isActual = (data[4] == "0") ? false : true
+                };
+
+                foreach (var tourType in data[5].Split(new string[] {","}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var currentType = ToursEntities.GetContext().Type.ToList().FirstOrDefault(p => p.name == tourType);
+                    if (currentType != null)
+                        tempTour.Type.Add(currentType);
+                }
+                try
+                {
+                    tempTour.imagePreview = File.ReadAllBytes(images.FirstOrDefault(p => p.Contains(tempTour.name)));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                ToursEntities.GetContext().Tour.Add(tempTour);
+                ToursEntities.GetContext().SaveChanges();
+            }
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
